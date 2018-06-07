b725a98ab6522c6be9b9a0fdcef629055b825f8c
ACSENT-PC\acsen <a.sitnikov@outlook.com>
2018-06-07 13:51:52 +0300

    navbar

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classNames from 'classnames'
 
+import { Navbar, Nav, NavItem, Button, FormGroup, FormControl } from 'react-bootstrap'
+
 import './nav_bar.css'
 
 const NavBar = (props) => {
 
     const menu = [
-        { name: 'Форум', link: `${window.hash}` },
+        { name: 'Форум', link: `${window.hash}/` },
         { name: '1С:Предприятие', link: `${window.hash}/index.php?forum=1C` },
         { name: 'Работа', link: `${window.hash}/index.php?forum=JOB` },
         { name: 'Wiki', link: 'http://wiki.mista.ru' },
@@ -14,17 +16,35 @@ const NavBar = (props) => {
         { name: 'Каталог разработок', link: 'http://catalog.mista.ru' }
     ];
 
-    let current = 0;
+    let currentItem = 0;
 
     const menuItems = menu.map((item, i) => {
-        const classes = classNames('nav-item', { 'active': i === current })
+        const classes = classNames('nav-item')
         return (
-            <li key={i} className={classes}>
-                <a className='inline-block' href={item.link}>{item.name}</a>
-            </li>
+            <NavItem eventKey={1} href={item.link}>{item.name}</NavItem>
         )
     });
 
+    return (
+        <Navbar fluid inverse>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Nav>
+                {menuItems}
+                </Nav>
+                <Navbar.Form pullRight>
+                    <FormGroup bsSize="sm">
+                        <FormControl componentClass="select">
+                            <option value="yandex">Яндекс</option>
+                            <option value="google">Google</option>
+                        </FormControl>{' '}
+                        <FormControl type="text" placeholder="Поиск"/>{' '}
+                        <Button type="submit" bsSize="sm">Найти</Button>
+                    </FormGroup>
+                </Navbar.Form>
+            </Navbar.Collapse>
+        </Navbar>
+    )
     return (
         <div id='header' className="header">
             <ul className="nav-bar">
