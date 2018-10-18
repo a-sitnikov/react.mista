2bc9734910d3a66d84f0c801475e9c12053a1c17
ACSENT-PC\acsen <a.sitnikov@outlook.com>
2018-10-18 18:23:53 +0300

    .

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,27 @@ import {createSerializer} from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+};
+
+global.localStorage = new LocalStorageMock;
