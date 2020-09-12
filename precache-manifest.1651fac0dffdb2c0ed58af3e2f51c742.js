3d237d50d65785c05f90001a5b923fef306b9508
a.sitnikov <a.sitnikov@outlook.com>
2020-09-12 10:48:14 +0300

    Updates

---
+++ precache-manifest.1651fac0dffdb2c0ed58af3e2f51c742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d613cbdf13d32fcf8d3eb7100bb7ff2",
+    "url": "/react.mista/index.html"
+  },
+  {
+    "revision": "0a8d74df5872cb6563bc",
+    "url": "/react.mista/static/css/main.6e8eb38c.chunk.css"
+  },
+  {
+    "revision": "9d99699f44ac9f43ad1b",
+    "url": "/react.mista/static/js/2.254999e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react.mista/static/js/2.254999e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8d74df5872cb6563bc",
+    "url": "/react.mista/static/js/main.7e7a0b3d.chunk.js"
+  },
+  {
+    "revision": "62d14fac076720ffeeca",
+    "url": "/react.mista/static/js/runtime-main.f77ae511.js"
+  }
+]);
