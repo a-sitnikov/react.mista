793422d11e64c95fe81a228816dd62d842ba1a3b
a.sitnikov <a.sitnikov@outlook.com>
2022-03-29 13:37:50 +0300

    .

--- src/components/topic/header.js
+++ src/components/topic/header.js
@@ -8,33 +8,28 @@ type Props = {
   info: { forum: string }
 }
 
-class Header extends React.Component<Props> {
+const Header = ({ info }) => {
 
-  render() {
+  const forums = {
+    '1c': '1С:Предприятие',
+    'life': 'О жизни',
+    'it': 'Информационные технологии',
+    'job': 'Работа'
+  };
 
-    const { info } = this.props;
+  return (
 
-    const forums = {
-      '1c': '1С:Предприятие',
-      'life': 'О жизни',
-      'it': 'Информационные технологии',
-      'job': 'Работа'
-    };
-
-    return (
-
-      <div className="flex-row">
-        <div className="user-td">
-          <Login />
-        </div>
-        <div className="section-td">
-          <span id="forum_string" className="bold120">
-            <a rel="nofollow" href={`${window.hash}/index.php?forum=${info.forum}`} style={{ textDecoration: "none" }}>{forums[info.forum]}</a>
-          </span>
-        </div>
+    <div className="flex-row">
+      <div className="header-left">
+        <Login />
       </div>
-    )
-  }
+      <div className="header-right">
+        <span id="forum_string" className="bold120">
+          <a rel="nofollow" href={`${window.hash}/index.php?forum=${info.forum}`} style={{ textDecoration: "none" }}>{forums[info.forum]}</a>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
