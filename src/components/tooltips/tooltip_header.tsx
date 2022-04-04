bb4d13f4ffbdd49b7098c069f9ec565a4a53bb6f
a.sitnikov <a.sitnikov@outlook.com>
2022-04-04 12:06:37 +0300

    .

--- src/components/tooltips/tooltip_header.tsx
+++ src/components/tooltips/tooltip_header.tsx
@@ -1,18 +1,19 @@
 import { FC, ReactElement } from 'react'
 
-type IProps ={
-    closeWindow?: (e: any) => any
+type IProps = {
+  closeWindow?: (e: any) => any
 }
 
-const TooltipHeader: FC<IProps> = ({children, closeWindow}): ReactElement => {
-    return (
-        <div className="tooltip-header">
-            {children}
-            <div className="tooltip-close" onClick={closeWindow} onTouchEnd={closeWindow}>
-                <span className="tooltip-close-x">x</span>
-            </div>
-        </div>
-    )
+const TooltipHeader: FC<IProps> = ({ children, closeWindow }): ReactElement => {
+  return (
+    <div className="tooltip-header">
+      {children}
+      <div className="tooltip-close" onClick={closeWindow} onTouchEnd={closeWindow}>
+        <i className="fa fa-angle-right" aria-hidden="true"></i>
+        <i className="fa fa-angle-left" aria-hidden="true" style={{ marginLeft: "-2px" }}></i>
+      </div>
+    </div>
+  )
 }
 
 export default TooltipHeader;
