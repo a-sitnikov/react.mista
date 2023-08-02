c8cc0648da830a0c6072d13e8c502639b3ac92c6
a.sitnikov <a.sitnikov@outlook.com>
2023-08-02 14:32:03 +0300

    .

---
+++ src/stories/topic/code1c.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+import Code1c from 'src/components/extensions/code1c';
+import 'src/components/extensions/code1c.css'
+
+import { text1 } from 'src/tests/mock_data/mock_code1c'
+
+const meta = {
+  title: 'topic/code1c',
+  component: Code1c,
+  parameters: {
+    //layout: 'centered',
+  },
+  tags: [],
+  argTypes: {
+  },
+} satisfies Meta<typeof Code1c>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Desktop: Story = {
+  args: {
+    children: [text1]
+  }
+};
+
+export const Mobile: Story = {
+  args: {
+    children: [text1]
+  },
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone6',
+    },
+  }  
+};
+
+export const Mobile_Dark: Story = {
+  args: {
+    children: [text1]
+  },
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone6',
+    },
+  },
+  decorators: [(story) => <div data-theme="dark">{story()}</div>]  
+};
+
