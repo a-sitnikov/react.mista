3be26dc0b643cb20029d8738dcdb724c011066cc
Anatoliy Sitnikov <a.sitnikov@outlook.com>
2025-08-27 17:49:34 +0300

    replace webpack to vite

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"), // Alias 'src' to the absolute path of your src directory
+    },
+  },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            // Default vendor chunk for other node_modules
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
