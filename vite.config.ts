8b32119f51a44109b8320c76968901abef173b3b
Anatoliy Sitnikov <a.sitnikov@outlook.com>
2025-08-27 21:13:46 +0300

    vite bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,39 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "./src"), // Alias 'src' to the absolute path of your src directory
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      mode === "analyze" &&
+        (visualizer({
+          open: true,
+        }) as PluginOption),
+    ],
+    resolve: {
+      alias: {
+        // Alias 'src' to the absolute path of your src directory
+        src: path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            // Default vendor chunk for other node_modules
-            return "vendor";
-          }
+    build: {
+      outDir: "build",
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes("node_modules")) {
+              // Default vendor chunk for other node_modules
+              return "vendor";
+            }
+          },
         },
       },
     },
-  },
-  server: {
-    port: 3000,
-  },
-  base: "/react.mista/",
+    server: {
+      port: 3000,
+    },
+    base: "/react.mista/",
+  };
 });
